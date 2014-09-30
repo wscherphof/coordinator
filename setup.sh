d9274f15d5851844bbfdd2b7a267ea3a3985214e
Wouter Scherphof <wsf@MBP-van-Wouter.local>
2014-09-30 12:35:56 +0200

    one pre packs data volume container

--- setup.sh
+++ setup.sh
@@ -19,31 +19,32 @@ while :; do
 	fi
 done
 
-echo "Creating jBoss prepack /jbpp data volume container"
-if [ ! -f bc_jbossprepack-R4.7.0.zip ]; then
-  wget http://releases.hq.ismobile.com/packages/coordinator-R4.7/bc_jbossprepack-R4.7.0.zip
-fi
-mkdir jbpp
-docker run --name jbpp -v $(pwd)/jbpp:/jbpp busybox true
-docker run --rm -v $(pwd)/bc_jbossprepack-R4.7.0.zip:/pp.zip --volumes-from jbpp busybox unzip /pp.zip -d /jbpp
-cp ojdbc7.jar jbpp/jboss-4.2.3.GA/server/coord/coordConfig/database/oracle-libs
-
-echo "Creating SwiftMQ prepack /smqpp data volume container"
+echo "Loading the prepacks in a data volume container"
+mkdir pp
+docker run --name pp -v $(pwd)/pp:/pp busybox true
+# SwiftMQ
 if [ ! -f bc_swiftmqprepack-R4.7.0.zip ]; then
   wget http://releases.hq.ismobile.com/packages/coordinator-R4.7/bc_swiftmqprepack-R4.7.0.zip
 fi
-mkdir smqpp
-docker run --name smqpp -v $(pwd)/smqpp:/smqpp busybox true
-docker run --rm -v $(pwd)/bc_swiftmqprepack-R4.7.0.zip:/pp.zip --volumes-from smqpp busybox unzip /pp.zip -d /smqpp
+mkdir pp/smq
+docker run --rm -v $(pwd)/bc_swiftmqprepack-R4.7.0.zip:/pp.zip --volumes-from pp busybox unzip /pp.zip -d /pp/smq
+# jBoss
+if [ ! -f bc_jbossprepack-R4.7.0.zip ]; then
+  wget http://releases.hq.ismobile.com/packages/coordinator-R4.7/bc_jbossprepack-R4.7.0.zip
+fi
+mkdir pp/jboss
+docker run --rm -v $(pwd)/bc_jbossprepack-R4.7.0.zip:/pp.zip --volumes-from pp busybox unzip /pp.zip -d /pp/jboss
+# also include the java 7 ojdbc jar
+cp ojdbc7.jar pp/jboss/jboss-4.2.3.GA/server/coord/coordConfig/database/oracle-libs
 
 echo "Creating database schemas"
-docker run --rm -v $(pwd)/db:/setup --volumes-from jbpp --link orcl:db guywithnose/sqlplus /setup/install
+docker run --rm -v $(pwd)/db:/setup --volumes-from pp --link orcl:db guywithnose/sqlplus /setup/install
 
 echo "Creating HpDoc heap"
-docker run --rm -v $(pwd)/heap:/setup --volumes-from jbpp --link orcl:db tifayuki/java:7 /setup/create
+docker run --rm -v $(pwd)/heap:/setup --volumes-from pp --link orcl:db tifayuki/java:7 /setup/create
 
 echo "Installing the jBoss & SwiftMQ packages in a base Java image"
-docker run --name base -v $(pwd)/setup:/setup --volumes-from jbpp --volumes-from smqpp tifayuki/java:7 /setup/install
+docker run --name base -v $(pwd)/setup:/setup --volumes-from pp tifayuki/java:7 /setup/install
 docker commit base coord:base
 docker rm base
 echo "Removing existing Coordinator container & image"
@@ -55,11 +56,9 @@ docker build -t coord coord
 echo "Removing the intermediairy Java image"
 docker rmi coord:base
 
-echo "Removing the data volumes"
-docker rm -v jbpp
-rm -rf jbpp
-docker rm -v smqpp
-rm -rf smqpp
+echo "Removing the prepacks data volume"
+docker rm -v pp
+rm -rf pp
 
 echo ""
 echo "Done. Now: $(date) - Started: $START"
